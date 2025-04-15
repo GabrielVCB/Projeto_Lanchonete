45a2cddb190cbf17018abbbe4e697b8fd1372230
gabrielvcb7 <gabriel.00000849430@unicap.br>
2025-04-15 14:10:38 -0300

    Botao voltar

--- shared.js
+++ shared.js
@@ -2,18 +2,6 @@ let cart = [];
 let currentOrder = [];
 let orderHistory = [];
 
-function addToCart(product) {
-  const itemIndex = cart.findIndex(item => item.name === product.name);
-  if (itemIndex !== -1) {
-    cart[itemIndex].quantity += 1;
-  } else {
-    cart.push({ ...product, quantity: 1 });
-  }
-  saveCart();
-  updateCartDisplay();
-  showNotification(`${product.name} foi adicionado ao carrinho`);
-}
-
 function saveCart() {
   localStorage.setItem('cart', JSON.stringify(cart));
 }
